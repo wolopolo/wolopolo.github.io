b4d568dcf3aa0f43b83b35c49f670c76a3ea2ca7
wolo <wolonguyen@gmail.com>
2023-01-27 16:40:04 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Welcome
+[wolopolo](https://wolopolo.github.io/)
+# Using
+- HTML/CSS/JS
+- Fontawesome
+- Support by github/giscus
---
+++ assert/script/main.js
@@ -0,0 +1,26 @@
+function showNavigation() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+// for scroll to top button
+window.onscroll = function () { showScrollButton() };
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+function showScrollButton() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
